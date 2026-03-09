36099d24e478ee76b1df907587fcb1c68ae51341
Audrius Kalpokas <akalpokas16@gmail.com>
2026-03-09 14:20:47 +0000

    Fix _dynamics.py key bug

--- src/sire/mol/_dynamics.py
+++ src/sire/mol/_dynamics.py
@@ -492,6 +492,7 @@ class DynamicsData:
                         zip(lambda_windows, rest2_scale_factors)
                     ):
                         if lambda_value != sim_lambda_value:
+                            key = f"{lambda_value:.5f}"
                             if (
                                 not has_lambda_index
                                 or abs(lambda_index - i) <= num_energy_neighbours
